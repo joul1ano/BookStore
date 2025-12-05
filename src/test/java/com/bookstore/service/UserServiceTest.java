b59ed9cef77f952a1a5ada45bd77f02e6cc39223
Tzouliano Giazai <tzoulianogiazai@gmail.com>
2025-12-05 17:12:27 +0200

    Finished UserService tests

--- src/test/java/com/bookstore/service/UserServiceTest.java
+++ src/test/java/com/bookstore/service/UserServiceTest.java
@@ -1,7 +1,9 @@
 package com.bookstore.service;
 
 import com.bookstore.DTOs.UserDTO;
+import com.bookstore.DTOs.UserMeDTO;
 import com.bookstore.enums.Role;
+import com.bookstore.exceptions.ResourceNotFoundException;
 import com.bookstore.mappers.UserMapper;
 import com.bookstore.model.User;
 import com.bookstore.repository.UserRepository;
@@ -17,6 +19,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.mockito.Mockito.*;
 
@@ -107,5 +110,132 @@ public class UserServiceTest {
         verify(userRepository).findAll();
         verify(userMapper, never()).toAdminDTO(any());
     }
+
+    @Test
+    @DisplayName("Get user by id - Success")
+    void getUserById_Found(){
+        User user = User.builder()
+                .id(1l)
+                .name("John")
+                .username("john7")
+                .password("123")
+                .email("johnex@gmail.com")
+                .phoneNumber("699999999")
+                .role(Role.USER)
+                .totalAmountSpent(0)
+                .createdAt(LocalDateTime.of(2025,12,4,16,0))
+                .lastLoginAt(LocalDateTime.of(2025,12,4,19,0))
+                .build();
+        UserDTO userDTO = UserDTO.builder()
+                .id(1l)
+                .name("John")
+                .username("john7")
+                .email("johnex@gmail.com")
+                .phoneNumber("699999999")
+                .role(Role.USER)
+                .totalAmountSpent(0)
+                .createdAt(LocalDateTime.of(2025,12,4,16,0))
+                .lastLoginAt(LocalDateTime.of(2025,12,4,19,0))
+                .build();
+
+        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+        when(userMapper.toAdminDTO(user)).thenReturn(userDTO);
+
+        UserDTO returnedUser = userService.getUserById(1L);
+
+        Assertions.assertNotNull(returnedUser);
+        Assertions.assertEquals(userDTO,returnedUser);
+
+        verify(userRepository).findById(1L);
+        verify(userMapper).toAdminDTO(user);
+    }
+
+    @Test
+    @DisplayName("Get user by id - Fail - Not found")
+    void testGetUserById_NotFound(){
+        when(userRepository.findById(1L)).thenReturn(Optional.empty());
+
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> userService.getUserById(1L));
+
+        verify(userRepository).findById(1L);
+        verify(userMapper, never()).toAdminDTO(any());
+    }
+
+    @Test
+    @DisplayName("Get user by email - Success")
+    void testGetUserByEmail_Found(){
+        User user = User.builder()
+                .id(1l)
+                .name("John")
+                .username("john7")
+                .password("123")
+                .email("johnex@gmail.com")
+                .phoneNumber("699999999")
+                .role(Role.USER)
+                .totalAmountSpent(0)
+                .createdAt(LocalDateTime.of(2025,12,4,16,0))
+                .lastLoginAt(LocalDateTime.of(2025,12,4,19,0))
+                .build();
+        UserMeDTO userDTO = UserMeDTO.builder()
+                .name("John")
+                .username("john7")
+                .email("johnex@gmail.com")
+                .phoneNumber("699999999")
+                .role(Role.USER)
+                .build();
+
+        when(userRepository.findByEmail("johnex@gmail.com")).thenReturn(Optional.of(user));
+        when(userMapper.toUserMeDTO(user)).thenReturn(userDTO);
+
+        UserMeDTO returnedUser = userService.getUserByEmail("johnex@gmail.com");
+
+        Assertions.assertNotNull(returnedUser);
+        Assertions.assertEquals(userDTO,returnedUser);
+
+        verify(userRepository).findByEmail("johnex@gmail.com");
+        verify(userMapper).toUserMeDTO(user);
+    }
+
+    @Test
+    @DisplayName("Get user by email - Fail - Not found")
+    void testGetUserByEmail_NotFound(){
+        when(userRepository.findByEmail("johnex@gmail.com")).thenReturn(Optional.empty());
+
+
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> userService.getUserIdByEmail("johnex@gmail.com"));
+
+        verify(userRepository).findByEmail("johnex@gmail.com");
+        verify(userMapper,never()).toUserMeDTO(any());
+
+    }
+
+    @Test
+    @DisplayName("Get a user's id by email - Success")
+    void testGetUserIdByEmail_Found(){
+        User user = User.builder()
+                .id(1l)
+                .name("John")
+                .username("john7")
+                .password("123")
+                .email("johnex@gmail.com")
+                .phoneNumber("699999999")
+                .role(Role.USER)
+                .totalAmountSpent(0)
+                .createdAt(LocalDateTime.of(2025,12,4,16,0))
+                .lastLoginAt(LocalDateTime.of(2025,12,4,19,0))
+                .build();
+
+        when(userRepository.findByEmail("johnex@gmail.com")).thenReturn(Optional.of(user));
+
+        Long id = userService.getUserIdByEmail("johnex@gmail.com");
+
+        Assertions.assertEquals(1L,id);
+
+        verify(userRepository).findByEmail("johnex@gmail.com");
+    }
+    /*
+    It doesn't make sense to write a negative test for this case, because when the method we test runs,
+    the user is already authenticated, which means he has logged in with a valid email, and he obviously has a valid user id
+     */
 
 }
