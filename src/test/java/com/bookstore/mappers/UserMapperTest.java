4d8daf568ef2b0ecbefb5fccfc9d40b002a63626
Tzouliano Giazai <tzoulianogiazai@gmail.com>
2025-12-06 20:06:24 +0200

    Completed user service tests

--- src/test/java/com/bookstore/mappers/UserMapperTest.java
+++ src/test/java/com/bookstore/mappers/UserMapperTest.java
@@ -1,6 +1,7 @@
 package com.bookstore.mappers;
 
 import com.bookstore.DTOs.UserDTO;
+import com.bookstore.DTOs.UserMeDTO;
 import com.bookstore.enums.Role;
 import com.bookstore.model.User;
 import org.junit.jupiter.api.Assertions;
@@ -9,6 +10,8 @@ import org.junit.jupiter.api.Test;
 import org.mapstruct.factory.Mappers;
 
 import java.time.LocalDateTime;
+
+import static org.mockito.Mockito.verify;
 
 public class UserMapperTest {
     private final UserMapper userMapper = Mappers.getMapper(UserMapper.class);
@@ -42,7 +45,35 @@ public class UserMapperTest {
         UserDTO returnedDTO = userMapper.toAdminDTO(user1);
 
         Assertions.assertEquals(userDTO1,returnedDTO);
+    }
 
+    @Test
+    @DisplayName("Map a user to userMeDTO success")
+    void testMapUserToUserMeDTO(){
+        User user = User.builder()
+                .id(1l)
+                .name("John")
+                .username("john7")
+                .password("123")
+                .email("johnex@gmail.com")
+                .phoneNumber("699999999")
+                .role(Role.USER)
+                .totalAmountSpent(0)
+                .createdAt(LocalDateTime.of(2025,12,4,16,0))
+                .lastLoginAt(LocalDateTime.of(2025,12,4,19,0))
+                .build();
+        UserMeDTO userDTO = UserMeDTO.builder()
+                .name("John")
+                .username("john7")
+                .email("johnex@gmail.com")
+                .phoneNumber("699999999")
+                .role(Role.USER)
+                .build();
 
+        UserMeDTO returnedUser = userMapper.toUserMeDTO(user);
+
+        Assertions.assertNotNull(returnedUser);
+        Assertions.assertEquals(userDTO,returnedUser);
+        
     }
 }
