becc0a1db244959ae11e1a4a463b3dc64db80dbe
Tzouliano Giazai <tzoulianogiazai@gmail.com>
2026-02-01 08:12:18 +0200

    Security changes

--- src/main/java/com/bookstore/config/JwtService.java
+++ src/main/java/com/bookstore/config/JwtService.java
@@ -6,6 +6,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +19,8 @@ import java.util.function.Function;
 
 @Service
 public class JwtService {
-    private static final String SECRET_KEY = "EEFBE830C6176265F734CC57F50F82E9D7D8AC190FA67B72F5FFC9BEA23B12BB";
+    @Value("${jwt.secret}")
+    private String SECRET_KEY;
 
 
     public String extractUsername(String token) {
